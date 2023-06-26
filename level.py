3cb3342c1220d5075b88b1d205f4bf374057ae1c
ottogibbon <113352402+ottogibbon@users.noreply.github.com>
2023-06-26 18:29:17 +0100

    2nd

--- level.py
+++ level.py
@@ -1,17 +1,34 @@
 import pygame
 from tiles import Tile
 from settings import tile_size
+from player import Player
 
 class Level:
     def __init__(self,level_data,surface):
         self.display_surface = surface
         self.setup_level(level_data)
+        self.world_shift = 0
 
     def setup_level(self,layout):
+        self.player = pygame.sprite.GroupSingle()
         self.tiles = pygame.sprite.Group()
         for row_index,row in enumerate(layout):
             for col_index,block in enumerate(row):
                 if block == 'X':
-                    print(f'{row_index}{col_index}:{block}')
+                    x = col_index * tile_size
+                    y = row_index * tile_size
+                    tile = Tile((x,y),tile_size)
+                    self.tiles.add(tile)
+                if block == 'P':
+                    x = col_index * tile_size
+                    y = row_index * tile_size
+                    player_sprite = Player((x,y))
+                    self.player.add(player_sprite)
+
     def run(self):
         self.tiles.draw(self.display_surface)
+        self.tiles.update(self.world_shift)
+
+        #player
+        self.player.update()
+        self.player.draw(self.display_surface)
